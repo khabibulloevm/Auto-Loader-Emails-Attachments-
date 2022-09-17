b50d3cf14043e57ccd7627083bf664cbab827f47
Khabibulloev Mukhammadsodik <86304910+khms9911@users.noreply.github.com>
2022-09-17 18:59:05 +0300

    Update and rename Mail.cs to MailItem.cs

---
+++ Models/MailItem.cs
@@ -0,0 +1,11 @@
+namespace EmailReseiver.Models
+{
+    public class MailItem
+    {
+        public string Date { get; set; }
+        public string From { get; set; }
+        public object Subj { get; set; }
+        public bool HasAttachments { get; set; }
+
+    }
+}
